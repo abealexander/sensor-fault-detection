e902a216baf18ddfeb5cc7c3b5a451643e884687
abealexander07 <abealexander07@gmail.com>
2022-11-05 15:05:26 +0530

    main.py updated

---
+++ main.py
@@ -0,0 +1,9 @@
+from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact
+from sensor.pipeline.training_pipeline import TrainPipeline
+
+if __name__ == "__main__":
+
+    train_pipeline = TrainPipeline()
+    train_pipeline.run_pipeline()
