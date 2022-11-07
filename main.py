978e00dc4a16772c4a16d5ef79295feb8b6cb50f
abealexander07 <abealexander07@gmail.com>
2022-11-07 22:23:10 +0530

    Updated

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from sensor.configuration.mongo_db_connection import MongoDBClient
-from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
-from sensor.entity.artifact_entity import DataIngestionArtifact
 from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.logger import logging
 
 if __name__ == "__main__":
-
-    train_pipeline = TrainPipeline()
-    train_pipeline.run_pipeline()
+    try:
+        train_pipeline = TrainPipeline()
+        train_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
